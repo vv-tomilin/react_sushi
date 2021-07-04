c4fc1e443bd77dd371b8b14c49198358c14a5c89
vv_tomilin <backatom1@gmail.com>
2021-07-04 15:38:34 +0500

    Commit 1 (предварительная верстка главной страницы)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Home from './pages/Home';
+
 function App() {
   return (
     <div>
-      <h1>Test</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/assets/logo and icons/index.js
@@ -0,0 +1,8 @@
+import logo from './logo.svg';
+import cityIcon from './location_ico.svg';
+import cartIcon from './cart_icon 1.svg';
+import filterIcon from './filter_ico.svg';
+import filterArrow from './filter_popup_arrow.svg';
+import footerLogo from './footer_logo.svg';
+
+export {logo, cityIcon, cartIcon, filterIcon, filterArrow, footerLogo};
