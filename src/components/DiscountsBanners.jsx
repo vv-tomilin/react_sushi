acc1e1a99ae994cc14305165eb8bd8d1a9a0f58a
vv_tomilin <backatom1@gmail.com>
2021-07-07 22:19:12 +0500

    Commit 11 (стилизовал слайдер баннеров со скидками и акциями)

--- src/components/DiscountsBanners.jsx
+++ src/components/DiscountsBanners.jsx
@@ -4,14 +4,32 @@ import bannerMob from '../assets/images/banner_mob.jpg';
 
 function DiscountsBanners() {
   return (
-    <section>
+    <section className='discounts-bunners'>
       <h2 className='visually-hidden'>Баннеры проходящих акций и скидок</h2>
-      <img src={bannerMob} />
-      <div>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+
+      <ul>
+        <li>
+          <img className='discounts-bunners__bunner' src={bannerMob} alt=''/>
+        </li>
+      </ul>
+
+      <div className='discounts-bunners__toggles-wrapper'>
+        <ul className='discounts-bunners__toggles'>
+          <li className='discounts-bunners__toggle'>
+            <button className='visually-hidden'></button>
+          </li>
+          <li
+            className='discounts-bunners__toggle
+            discounts-bunners__toggle_current'>
+            <button className='visually-hidden'></button>
+          </li>
+          <li className='discounts-bunners__toggle'>
+            <button className='visually-hidden'></button>
+          </li>
+          <li className='discounts-bunners__toggle'>
+            <button className='visually-hidden'></button>
+          </li>
+        </ul>
       </div>
     </section>
   );
