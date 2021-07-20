8daf4b2330469769af820661f9da9e1e2a0ad303
vv_tomilin <backatom1@gmail.com>
2021-07-20 19:18:11 +0500

    Commit 26.1 (сделал отображение общего количества добавленных в корзину продуктов и их общей цены на кнопке корзины)

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {useSelector} from 'react-redux';
+
 import {cartIcon} from '../assets/logo and icons';
 
 function CartButton({className}) {
+  const {totalPrice, totalCount} = useSelector(({cart}) => cart);
+
   return (
     <div className={`${className}_mobile`}>
       <div className={className}>
         <a href='#' >
-          <span className={`${className}__price`}>0₽</span>
+          <span className={`${className}__price`}>{totalPrice}₽</span>
           <img src={cartIcon} alt='cart_icon' />
           <div className={`${className}__count-wrapper`}>
-            <span className={`${className}__count`}>0</span>
+            <span className={`${className}__count`}>{totalCount}</span>
           </div>
           <span className='visually-hidden'>Корзина</span>
         </a>
