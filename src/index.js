a6c0cf7e807caa6ee1976608f5881f8c89ce7e36
vv_tomilin <backatom1@gmail.com>
2021-07-26 08:57:36 +0500

    Commit 28 (подключил маршрутизацию через библиотеку _react-router-dom_)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import {Provider} from 'react-redux';
-
 import store from './redux/store';
 
 import App from './App';
@@ -11,9 +11,11 @@ import './scss/app.scss';
 
 ReactDOM.render(
     <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <Router>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </Router>
     </React.StrictMode>,
     document.getElementById('root'),
 );
