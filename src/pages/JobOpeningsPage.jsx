14f4b7dedf87a1ed83fb8c2121576b48106e8a20
vv_tomilin <backatom1@gmail.com>
2021-08-08 14:01:21 +0500

    Commit 40.3 (наполнил компонент _JobOpeningsPage_ контентом и прописал для него стили)

--- src/pages/JobOpeningsPage.jsx
+++ src/pages/JobOpeningsPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function JobOpeningsPage() {
   return (
     <div className='job-openings-page'>
-      <div>Job Openings</div>
+      <div className='job-openings-page__content-wrapper'>
+        <h1 className='job-openings-page__title'>НАШИ ВАКАНСИИ</h1>
+        <p className='job-openings-page__smile'>&#128577;</p>
+        <p className='job-openings-page__text'>
+          К сожалению на данный момент открытых вакансий нет...
+        </p>
+        <div className='job-openings-page__btn-back-wrapper'>
+          <Link to='/'>
+            <button className='cart__empty-btn-back'>
+                Вернуться на главную
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
